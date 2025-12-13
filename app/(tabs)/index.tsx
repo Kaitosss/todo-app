3830ec75a6521009fd2d33a0427b541918d50533
Kaito <nuruto123456789za@gmail.com>
2025-12-13 20:02:17 +0700

    add home styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
+import createHomeStyles from "@/assets/styles/home.styles";
+import { useTheme } from "@/hooks/useTheme";
 import { Text, View } from "react-native";
 
 const Index = () => {
+  const { colors } = useTheme();
+  const styles = createHomeStyles(colors);
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>index</Text>
     </View>
   );
 };
-
-export default Index;
