cdb0b43f38b179f1122b3fde8d3077f64235c25f
Kaito <nuruto123456789za@gmail.com>
2025-12-13 21:18:49 +0700

    add header component in home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,25 @@
 import createHomeStyles from "@/assets/styles/home.styles";
+import Header from "@/components/Header";
 import { useTheme } from "@/hooks/useTheme";
-import { Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Index = () => {
   const { colors } = useTheme();
-  const styles = createHomeStyles(colors);
+  const homeStyles = createHomeStyles(colors);
 
   return (
-    <View style={styles.container}>
-      <Text>index</Text>
-    </View>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={homeStyles.container}
+    >
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={homeStyles.safeArea}>
+        <Header />
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
+
+export default Index;
